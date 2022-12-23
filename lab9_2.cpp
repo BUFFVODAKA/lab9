9003b18bddc5000703aba3c755088996363b77cf
VODAKA <poottipong_k@cmu.ac.th>
2022-12-23 16:22:23 +0700

    lab9_2 finished

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,23 +3,26 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double x){
+    x = (x>90 ? 'A' : (x >75 && x<=90) ? 'B' : (x>60 && x<=75) ? 'C' : (x>45 && x<=60) ? 'D' : 'F' );
+    
+    return x;
+}
 
 int main(){
 	//Input the number of students
 	int N,i = 0;
 	cout << "Enter the number of students: ";
 	cin >> N;
-	string name[N];
 	float score[N];	
 	
 	//Store names and scores of students into an array 
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
